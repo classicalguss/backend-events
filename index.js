7652a1fd29e52ae61f78231814924e7abdfa9ca5
Ghassan Barghouti <ghassan@yasmina.ai>
2024-09-09 19:22:37 +0300

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const eventsRouter = require('./eventsRouter');
+const eventsRouter = require('./router');
 
 app.use('/events', eventsRouter);
 
