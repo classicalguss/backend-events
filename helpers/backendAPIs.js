a035c878d67b234462d83211c57aede993d77865
Ghassan Barghouti <ghassan@yasmina.ai>
2025-04-01 13:41:59 +0300

    zone api

--- helpers/backendAPIs.js
+++ helpers/backendAPIs.js
@@ -1,4 +1,5 @@
 const redisClient = require('./redisClient');
+const axios = require("axios");
 
 async function getFloorById(id, req) {
     const cacheKey = `floor:${id}`;
@@ -29,4 +30,33 @@ async function getFloorById(id, req) {
     }
 }
 
-module.exports = { getFloorById };
+async function getZoneById(id, req) {
+    const cacheKey = `zone:${id}`;
+
+    try {
+        // Check Redis first
+        const cachedData = await redisClient.get(cacheKey);
+        if (cachedData) {
+            return JSON.parse(cachedData);
+        }
+
+        // Fetch from API if not found in Redis
+        const axios = require('axios');
+        const response = await axios.get(`${process.env.BACKEND_DOMAIN}/api/v1/zone/${id}`,{
+            headers: {
+                "Authorization": req.get('authorization')
+            }
+        });
+        const zoneData = response.data.data;
+
+        // Store in Redis with expiration (1 hour)
+        await redisClient.set(cacheKey, JSON.stringify(zoneData), { EX: 10400 });
+
+        return zoneData;
+    } catch (error) {
+        console.error("Error fetching zone data:", error.message);
+        // throw error;
+    }
+}
+
+module.exports = { getFloorById, getZoneById };
--- helpers/extraInfoCollector.js
+++ helpers/extraInfoCollector.js
@@ -1,11 +1,14 @@
-const {getFloorById} = require("./backendAPIs");
+const {getFloorById, getZoneById} = require("./backendAPIs");
 
 async function extraInfoCollector(payload, req) {
     if (payload.details.type === "floor") {
         let floorDetails = await getFloorById(payload.details.floorId, req);
         delete floorDetails.rent_contracts;
         payload.extra_details = floorDetails;
+    } else if (payload.details.type === "zone") {
+        payload.extra_details = await getZoneById(payload.details.zoneId, req);
     }
+
     return payload;
 }
 
